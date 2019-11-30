20eb408bc953c74cbd36c819d8f303f7dec278a0
AEGarten <etherjack@gmail.com>
2019-11-30 13:54:32 -0800

    Interface now only shows upcoming appts

--- DrDocket/Patient.cpp
+++ DrDocket/Patient.cpp
@@ -5,11 +5,9 @@ using namespace std;
 
 void Patient::printAppts() {
 	ApptNode* current = nullptr;
-    Date day;
-    Time start;
-    Time length;
+    Date today = Date::getCurrentDate();
     int count = 1;
-    for (int i = 0; i < 53; ++i) {
+    for (int i = today.getWeekNum(); i < 53; ++i) {
         current = oblig[i][0];
         
         for (int j = 0; j < nodeInv[i][0]; ++j) {
